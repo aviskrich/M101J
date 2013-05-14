527e189aef40e92c74fa0dbfa6f328766b3b4f21
Andrey Iskrich <aviskrich@gmail.com>
2013-05-14 17:43:09 +0400

    MongoDB + Spark + Freemarker

--- Test/src/main/java/com/tengen/HelloWorldSparkStyle.java
+++ Test/src/main/java/com/tengen/HelloWorldSparkStyle.java
@@ -1,9 +1,19 @@
 package com.tengen;
 
+import com.mongodb.*;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,11 +23,29 @@ import spark.Spark;
  * To change this template use File | Settings | File Templates.
  */
 public class HelloWorldSparkStyle {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
+        MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
+
+        DB database = client.getDB("course");
+        final DBCollection collection = database.getCollection("hello");
+
         Spark.get(new Route("/") {
             @Override
             public Object handle(Request request, Response response) {
-                return "Hello World From Spark";
+                StringWriter writer = new StringWriter();
+
+                try {
+                    Configuration configuration = new Configuration();
+                    configuration.setClassForTemplateLoading(HelloWorldFreeMarkerStyle.class, "/");
+                    Template helloTemplate = configuration.getTemplate("hello.ftl");
+
+                    DBObject document = collection.findOne();
+                    helloTemplate.process(document, writer);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                return writer;
             }
         });
     }
